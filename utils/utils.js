485b2aab955050007f6cc8c28611edf65e85f769
Himanshu Negi <ecoworld007@gmail.com>
2018-04-23 18:07:07 +0530

    using expect library for assertion and added setName util

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,15 @@
 let add = (a, b) => a + b;
 
 let square = (a) => a*a;
+
+let setName = (user, fullName) => {
+    let names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+    return user;
+};
 module.exports = {
     add,
-    square
+    square,
+    setName
 };
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,24 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('Should add numbers', () => {
     let res = utils.add(2,5);
-    if(res !== 7){
-        throw new Error(`Expected 7 but got ${res}`);
-    }
+    expect(res).toBeA('number').toBe(7);
 });
 
 it('Should square number', () => {
     let res = utils.square(5);
-    if(res !== 25){
-        throw new Error(`Expected 25 but got ${res}`);
-    }
+    expect(res).toBeA('number').toBe(25);
+});
+
+it('Should set proper name for user', () => {
+    let user = {
+        age: 25
+    };
+    let res = utils.setName({},'Himanshu Negi');
+    expect(res).toBeA('object').toIncludeKeys(['firstName', 'lastName']).toInclude({
+        firstName: 'Himanshu',
+        lastName: 'Negi'
+    });
 });
